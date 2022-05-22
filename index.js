61394d00938e5312678f232f678a45300be54f6c
Henry Escobar <henrye@gmail.com>
2022-05-21 18:52:12 -0700

    commit tree 6aac7940bdbe48a6ac52d6b5e14c0d98a761bfc0

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// code your solution here
+function saturdayFun(activity){
+    if (!activity){
+        return "This Saturday, I want to roller-skate!";
+    }
+
+    else
+        return `This Saturday, I want to ${activity}!`;
+
+}
+
+function mondayWork(activity){
+    if (!activity){
+        return "This Monday, I will go to the office.";
+    }
+
+    else
+        return `This Monday, I will ${activity}.`;
+}
+
+
+function wrapAdjective(visual){
+    //Variable declaration and assignment for visual flair and adjective to use
+    //when one isn't passed as arguments into the functions.
+    let defaultAdjective = 'special';
+    let defaultVisual = '*';
+    //Returns the function receiving the adjective input.
+    return function(adjective){
+        //No visual flair entered.
+        if (!visual){
+            //No adjestive entered.
+            if (!adjective){
+                return `You are ${defaultVisual}${defaultAdjective}${defaultVisual}!`;
+            }
+            else
+                return `You are ${defaultVisual}${adjective}${defaultVisual}!`;
+        }
+        else
+            if (!adjective){
+                return `You are ${visual}${defaultAdjective}${visual}!`;
+            }
+            else
+                return `You are ${visual}${adjective}${visual}!`;
+    }
+}
